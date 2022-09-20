c49c0d326ae1bf8fb3ec7240e8f4afac34d68ca4
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-09-20 18:45:44 +0300

    Add getComputerChoice() function to script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Hello World!");
+function getComputerChoice() {
+    let choice = Math.ceil(Math.random()*3)
+    switch(choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+console.log(getComputerChoice());
