52433ad1cbb9c0e7af0da05164aef8c8345476b4
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-09-20 19:12:56 +0300

    Add playRound() function to script.js

--- script.js
+++ script.js
@@ -12,4 +12,51 @@ function getComputerChoice() {
             break;
     }
 }
-console.log(getComputerChoice());
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    console.log(`You chose: ${playerChoice}\nThe computer chose: ${computerChoice}`);
+    if(playerChoice === "rock") {
+        switch(computerChoice) {
+            case "rock":
+                return "Draw! Both you and the computer chose the same thing";
+                break;
+            case "paper":
+                return "You Lost! Paper beats Rock";
+                break;
+            case "scissors":
+                return "You Won! Rock beats Scissors";
+                break;
+        }
+    }
+    if(playerChoice === "paper") {
+        switch(computerChoice) {
+            case "rock":
+                return "You Won! Paper beats Rock";
+                break;
+            case "paper":
+                return "Draw! Both you and the computer chose the same thing";
+                break;
+            case "scissors":
+                return "You Lost! Scissors beats Paper"
+                break;
+        }
+    }
+    if(playerChoice == "scissors") {
+        switch(computerChoice) {
+            case "rock":
+                return "You Lost! Rock beats Scissors";
+                break;
+            case "paper":
+                return "You Won! Scissors beats Paper";
+                break;
+            case "scissors":
+                return "Draw! Both you and the computer chose the same thing";
+                break;
+        }
+    }
+    return "Something went wrong!";
+}
+let playerChoice = window.prompt("Rock, Paper or Scissors?");
+let computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
