048b413698b73f48ad1c1abb9d7a628432b9fbb9
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-10-04 18:55:37 +0300

    Refactor code and add UI
    Made the game work using a basic HTML UI. Also added a "play again"
    option that resets the game.

--- script.js
+++ script.js
@@ -13,77 +13,127 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerChoice, computerChoice) {
-    playerChoice = playerChoice.toLowerCase();
-    console.log(`You chose: ${playerChoice}\nThe computer chose: ${computerChoice}`);
-    if(playerChoice === "rock") {
-        switch(computerChoice) {
-            case "rock":
-                return "Draw! Both you and the computer chose the same thing";
-                break;
-            case "paper":
-                score--;
-                return "You Lost! Paper beats Rock";
-                break;
-            case "scissors":
-                score++;
-                return "You Won! Rock beats Scissors";
-                break;
-        }
-    }
-    if(playerChoice === "paper") {
-        switch(computerChoice) {
-            case "rock":
-                score++;
-                return "You Won! Paper beats Rock";
-                break;
-            case "paper":
-                return "Draw! Both you and the computer chose the same thing";
-                break;
-            case "scissors":
-                score--;
-                return "You Lost! Scissors beats Paper"
-                break;
-        }
-    }
-    if(playerChoice == "scissors") {
-        switch(computerChoice) {
-            case "rock":
-                score--;
-                return "You Lost! Rock beats Scissors";
-                break;
-            case "paper":
-                score++;
-                return "You Won! Scissors beats Paper";
-                break;
-            case "scissors":
-                return "Draw! Both you and the computer chose the same thing";
-                break;
-        }
-    }
-    return "Something went wrong!";
-}
+function playRound(e) {
+    if(playerScore < 3 && computerScore < 3) {
 
+        messageBox.appendChild(lastRound);
+        messageBox.appendChild(playerChoiceDiv);
+        messageBox.appendChild(computerChoiceDiv)
+        messageBox.appendChild(resultDiv);
+
+        const playerChoice = e.target.textContent.toLowerCase();
+        const computerChoice = getComputerChoice();
+        playerChoiceDiv.textContent = `You chose: ${playerChoice}`;
+        computerChoiceDiv.textContent = `Computer chose: ${computerChoice}`;
+
+        if(playerChoice === "rock") {
+            switch(computerChoice) {
+                case "rock":
+                    resultDiv.textContent = "Draw! Both you and the computer chose the same thing";
+                    break;
+                case "paper":
+                    computerScore++;
+                    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+                    resultDiv.textContent = "You Lost! Paper beats Rock";
+                    break;
+                case "scissors":
+                    playerScore++;
+                    playerScoreDiv.textContent = `Your Score: ${playerScore}`;
+                    resultDiv.textContent = "You Won! Rock beats Scissors";
+                    break;
+            }
+        }
+        if(playerChoice === "paper") {
+            switch(computerChoice) {
+                case "rock":
+                    playerScore++;
+                    playerScoreDiv.textContent = `Your Score: ${playerScore}`;
+                    resultDiv.textContent = "You Won! Paper beats Rock";
+                    break;
+                case "paper":
+                    resultDiv.textContent = "Draw! Both you and the computer chose the same thing";
+                    break;
+                case "scissors":
+                    computerScore++;
+                    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+                    resultDiv.textContent = "You Lost! Scissors beats Paper"
+                    break;
+            }
+        }
+        if(playerChoice == "scissors") {
+            switch(computerChoice) {
+                case "rock":
+                    computerScore++;
+                    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+                    resultDiv.textContent = "You Lost! Rock beats Scissors";
+                    break;
+                case "paper":
+                    playerScore++;
+                    playerScoreDiv.textContent = `Your Score: ${playerScore}`;
+                    resultDiv.textContent = "You Won! Scissors beats Paper";
+                    break;
+                case "scissors":
+                    resultDiv.textContent = "Draw! Both you and the computer chose the same thing";
+                    break;
+            }
+        }
+        if(playerScore === 3) {
+            finalDiv.textContent = "GAME OVER: You Won!";
+            document.body.appendChild(finalDiv);
+            playAgain();
+        }
+        else if(computerScore === 3) {
+            finalDiv.textContent = "GAME OVER: You Lost!";
+            document.body.appendChild(finalDiv);
+            playAgain();
+        }
+    }
+    
+}
+function playAgain() {
+
+    playAgainBtn.textContent = "Play Again";
+    document.body.appendChild(playAgainBtn);
+    playAgainBtn.addEventListener('click', playAgainListener);
+}
+function playAgainListener(e) {
+    messageBox.removeChild(lastRound);
+    messageBox.removeChild(playerChoiceDiv);
+    messageBox.removeChild(computerChoiceDiv);
+    messageBox.removeChild(resultDiv);
+    playerScore = 0;
+    playerScoreDiv.textContent = `Your Score: ${playerScore}`;
+    computerScore = 0;
+    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+    document.body.removeChild(finalDiv);
+    document.body.removeChild(e.target);
+    document.body.appendChild(messageBox);
+
+}
 function game() {
-    for(let i=1; i<=5; i++) {
-        let playerChoice = window.prompt("Rock, Paper or Scissors?");
-        let computerChoice = getComputerChoice();
-        console.log(`\nROUND ${i}:`)
-        console.log(playRound(playerChoice, computerChoice));
-    }
-    switch(true) {
-        case score==0:
-            console.log("\nFINAL RESULT: Draw! No one won");
-            break;
-        case score < 0:
-            console.log("\nFINAL RESULT: You Lost! The computer won");
-            break;
-        case score > 0:
-            console.log("\nFINAL RESULT: You Won!")
-            break;
-    }
-    console.log(`FINAL SCORE: ${score}`);
+    playerScoreDiv.textContent = `Your Score: ${playerScore}`;
+    computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+
+    const buttonsList = document.querySelectorAll("button");
+    buttonsList.forEach(function(button) {
+        button.addEventListener('click', playRound);
+    });
 }
 
-let score=0;
+//Global variables
+let playerScore = 0, computerScore = 0;
+const playerScoreDiv = document.querySelector(".playerScore");
+const computerScoreDiv = document.querySelector(".computerScore");
+
+const messageBox = document.querySelector(".message-section");
+
+const lastRound = document.createElement("p");
+lastRound.textContent = "Last Round:";
+const playerChoiceDiv = document.createElement("div");
+const computerChoiceDiv = document.createElement("div");
+const resultDiv = document.createElement("div");
+
+const finalDiv = document.createElement("div");
+const playAgainBtn = document.createElement("button");
+
 game();
