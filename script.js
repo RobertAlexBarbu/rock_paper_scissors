a4a90860df3487ca2eda7c7a8b2a772818fd7aa5
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-10-08 01:15:04 +0300

    Add CSS styles

--- script.js
+++ script.js
@@ -79,12 +79,12 @@ function playRound(e) {
         }
         if(playerScore === 3) {
             finalDiv.textContent = "GAME OVER: You Won!";
-            document.body.appendChild(finalDiv);
+            gameContainer.appendChild(finalDiv);
             playAgain();
         }
         else if(computerScore === 3) {
             finalDiv.textContent = "GAME OVER: You Lost!";
-            document.body.appendChild(finalDiv);
+            gameContainer.appendChild(finalDiv);
             playAgain();
         }
     }
@@ -93,7 +93,7 @@ function playRound(e) {
 function playAgain() {
 
     playAgainBtn.textContent = "Play Again";
-    document.body.appendChild(playAgainBtn);
+    gameContainer.appendChild(playAgainBtn);
     playAgainBtn.addEventListener('click', playAgainListener);
 }
 function playAgainListener(e) {
@@ -105,9 +105,9 @@ function playAgainListener(e) {
     playerScoreDiv.textContent = `Your Score: ${playerScore}`;
     computerScore = 0;
     computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
-    document.body.removeChild(finalDiv);
-    document.body.removeChild(e.target);
-    document.body.appendChild(messageBox);
+    gameContainer.removeChild(finalDiv);
+    gameContainer.removeChild(e.target);
+    gameContainer.appendChild(messageBox);
 
 }
 function game() {
@@ -119,15 +119,22 @@ function game() {
         button.addEventListener('click', playRound);
     });
 }
-
+function addStyles() {
+    lastRound.classList.add("lastRound");
+    resultDiv.classList.add("result");
+    finalDiv.classList.add("gameOver");
+    playAgainBtn.classList.add("againBtn");
+}
 //Global variables
+const gameContainer = document.body.querySelector(".game");
+
 let playerScore = 0, computerScore = 0;
 const playerScoreDiv = document.querySelector(".playerScore");
 const computerScoreDiv = document.querySelector(".computerScore");
 
 const messageBox = document.querySelector(".message-section");
 
-const lastRound = document.createElement("p");
+const lastRound = document.createElement("h3");
 lastRound.textContent = "Last Round:";
 const playerChoiceDiv = document.createElement("div");
 const computerChoiceDiv = document.createElement("div");
@@ -136,4 +143,5 @@ const resultDiv = document.createElement("div");
 const finalDiv = document.createElement("div");
 const playAgainBtn = document.createElement("button");
 
+addStyles();
 game();
